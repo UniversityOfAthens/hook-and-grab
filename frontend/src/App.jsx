1d5523da7e309372cbe9200b7201baa25146b988
= <=>
2024-12-03 20:52:15 +0200

    Created website footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,22 @@ import Profile from './pages/Profile';
 import Market from './pages/Market';
 import Forum from './pages/Forum';
 import RentABoat from './pages/RentABoat';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/market" element={<Market />} />
-        <Route path="/forum" element={<Forum />} />
-        <Route path="/rentaboat" element={<RentABoat />} />
-      </Routes>
+    <div classname="app-c">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/market" element={<Market />} />
+          <Route path="/forum" element={<Forum />} />
+          <Route path="/rentaboat" element={<RentABoat />} />
+        </Routes>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
