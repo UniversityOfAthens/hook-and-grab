e9d66c6aacb0ef4b543307bfb809c91a43ca67ee
Stelios Rotas <steliosrotas@yahoo.com>
2024-12-06 11:38:16 +0200

    Temporary changes to the profile page.

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -8,12 +8,16 @@ import '../styles/Profile.css';
 
 function Profile() {
     const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [birth, setBirth] = useState('');
     const navigate = useNavigate();
 
     useEffect(() => {
         const user = JSON.parse(localStorage.getItem('user'));
         if (user) {
             setUsername(user.username);
+            setEmail(user.email);
+            setBirth(user.birth);
         }
     }, []);
 
@@ -35,27 +39,27 @@ function Profile() {
                             <h5 className='color-white'>Username: {username}</h5>
                             <div className='container'>
                                 <div className='box3'>
-                                    <h5 className='text-white'>Email: </h5>
-                                    <p className='text-white'>(TBA)</p>
-                                    <h5 className='text-white'>Posts:</h5>
-                                    <p className='text-white'>(TBA)</p>
+                                    <h5 className='text-white'>Email: {email}</h5>
+                                    {/* <p className='text-white'>{email}</p> */}
+                                    <h5 className='text-white'>Posts: 2</h5>
+                                    {/* <p className='text-white'>2</p> */}
                                 </div>
                                 <div className='box3'>
-                                    <h5 className='text-white'>Date of Birth: </h5>
-                                    <p className='text-white'>(TBA)</p>
-                                    <h5 className='text-white'>Location:</h5>
-                                    <p className='text-white'>(TBA)</p>
+                                    <h5 className='text-white'>Date of Birth: {birth}</h5>
+                                    {/* <p className='text-white'>{birth}</p> */}
+                                    <h5 className='text-white'>Location: Athens</h5>
+                                    {/* <p className='text-white'>Athens</p> */}
                                 </div>
                                 
                             </div>
                             <div className='container'>
                                 <div className='box4'>
-                                    <h5 className='text-white'>Purchases: </h5>
-                                    <p className='text-white'>(TBA)</p>
-                                    <h5 className='text-white'>Items Sold:</h5>
-                                    <p className='text-white'>(TBA)</p>
-                                    <h5 className='text-white'>Trades:</h5>
-                                    <p className='text-white'>(TBA)</p>
+                                    <h5 className='text-white'>Purchases: 5</h5>
+                                    {/* <p className='text-white'>5</p> */}
+                                    <h5 className='text-white'>Items Sold: 1</h5>
+                                    {/* <p className='text-white'>1</p> */}
+                                    <h5 className='text-white'>Trades: 2</h5>
+                                    {/* <p className='text-white'>2</p> */}
                                 </div>
                             </div>
                             <Button variant="primary" onClick={handleGoBack} className="button-style mt-3">Go back</Button>
