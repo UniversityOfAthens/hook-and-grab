d1f4cc33bf3fc75849b74faf7e043860d4cc2477
Karounos <vankarounos@gmail.com>
2024-12-07 09:28:58 +0200

    Separate about page created(with animation)

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import NavBar from './../components/NavBar';
+import '../styles/About.css'; 
+function About(){
+{/* About Section */}
+return(
+    <div className="about-container">
+        <NavBar />
+        <div className="box2">
+        <div className="header">
+            <h1>About</h1>
+            <p>At Hook&Grab, we’re reeling in a brighter future for our oceans and coastal communities. Rooted in the principles of the blue and circular economies, our mission is to create a platform where marine resources are reused, shared, and celebrated.</p>
+        </div>
+        </div>
+    </div>
+    
+    );
+};
+
+export default About;
